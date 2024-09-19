6b4b1c3e2064a99bd34b4e1ebd0ec1b27ae6a9c6
osaaxe-1 <oscar.axelstrom@hotmail.se>
2024-09-19 17:27:15 +0200

    Working test (kinda)

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -2,14 +2,27 @@ package ltu;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
+
+import static ltu.CalendarFactory.getCalendar;
+
 
 public class PaymentTest
 {
+    PaymentImpl payment;
     @Test
-    public void testSilly()
+    public void newPaymentObject()throws IOException{
+        payment = new PaymentImpl(getCalendar());
+    }
+
+
+    @Test
+    public void testNextPaymentDay() 
     {
-        assertEquals(1, 1);
+        assertEquals("20240930", payment.getNextPaymentDay());
+        
     }
 
 }
